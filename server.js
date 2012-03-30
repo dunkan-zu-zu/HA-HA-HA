89d2f9c5cdd6185f2d3a8ade89fb8092bd32bf7f
unknown <kll@tatarin-1.(none)>
2012-03-30 17:39:23 +0300

    1

---
+++ server.js
@@ -0,0 +1,23 @@
+var http=require('express').createServer();
+var url=require('url');
+var jade=require('jade');
+
+
+
+http.get('*',function(req,res){
+
+    Example=url.parse(req.url,true).query
+   jade.renderFile(__dirname+'/index.jade', {
+        options: 'here'
+    }, function (err, html) {
+
+        console.log(err);
+        res.send(html)
+
+    });
+
+
+    console.log(Example);
+});
+http.listen(3000);
+console.log('Сервер запущен');
